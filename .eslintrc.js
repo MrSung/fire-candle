21537491bea4ca76698eedf83bb69c45a70dc436
Sungjoon Park <sungjoon512@gmail.com>
2020-10-25 22:09:50 +0900

    setup eslint and prettier for typescript

---
+++ .eslintrc.js
@@ -0,0 +1,105 @@
+module.exports = {
+  ignorePatterns: ['**/node_modules/**', 'src/@types/**'],
+  env: {
+    browser: true,
+    es6: true,
+    node: true
+  },
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'plugin:prettier/recommended',
+    'plugin:react/recommended',
+    'plugin:jest/recommended',
+    'plugin:jest/style',
+    'prettier/react',
+    'prettier/standard'
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: 'tsconfig.json',
+    sourceType: 'module'
+  },
+  plugins: ['@typescript-eslint', 'jest'],
+  settings: {
+    react: {
+      version: 'detect'
+    }
+  },
+  rules: {
+    'no-console': ['error', { allow: ['error'] }],
+    '@typescript-eslint/naming-convention': [
+      'error',
+      {
+        selector: 'interface',
+        format: ['PascalCase'],
+        custom: {
+          regex: '^I[A-Z]',
+          match: true
+        }
+      }
+    ],
+    '@typescript-eslint/member-delimiter-style': [
+      'error',
+      {
+        multiline: {
+          delimiter: 'none',
+          requireLast: true
+        },
+        singleline: {
+          delimiter: 'semi',
+          requireLast: false
+        }
+      }
+    ],
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-unused-vars': [
+      'error',
+      { argsIgnorePattern: '^_$' }
+    ],
+    '@typescript-eslint/no-use-before-define': 'off',
+    '@typescript-eslint/unbound-method': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-unsafe-return': 'off',
+    '@typescript-eslint/no-unsafe-member-access': 'off',
+    '@typescript-eslint/no-unsafe-assignment': 'off',
+    '@typescript-eslint/no-unsafe-call': 'off',
+    '@typescript-eslint/ban-ts-comment': [
+      'error',
+      {
+        'ts-expect-error': false,
+        'ts-ignore': 'allow-with-description',
+        'ts-nocheck': true,
+        'ts-check': false,
+        minimumDescriptionLength: 3
+      }
+    ],
+    '@typescript-eslint/restrict-plus-operands': 'off',
+    '@typescript-eslint/restrict-template-expressions': [
+      'error',
+      {
+        allowNumber: true,
+        allowBoolean: false,
+        allowAny: true,
+        allowNullish: true
+      }
+    ],
+    'prettier/prettier': [
+      'error',
+      {
+        jsxSingleQuote: true,
+        singleQuote: true,
+        semi: false,
+        endOfLine: 'auto',
+        trailingComma: 'none',
+        arrowParens: 'avoid'
+      }
+    ],
+    'react/display-name': 'off',
+    'react/prop-types': 'off',
+    'jest/expect-expect': 'off',
+    'jest/no-export': 'off',
+    'jest/valid-describe': 'off',
+    'jest/no-commented-out-tests': 'off'
+  }
+}

c27e381352b459f4e75c48b456228366a40f4a69
Sungjoon Park <sungjoon512@gmail.com>
2020-10-30 23:42:08 +0900

    create and place app component

--- src/index.tsx
+++ src/index.tsx
@@ -2,8 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import reportWebVitals from './reportWebVitals'
 import './index.css'
-
-const App = () => <div>This is the app!</div>
+import { App } from './components/app'
 
 ReactDOM.render(
   <React.StrictMode>

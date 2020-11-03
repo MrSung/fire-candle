8a65fa5b4e7140f99b037216e8a291a8ee19a371
Sungjoon Park <sungjoon512@gmail.com>
2020-11-03 20:45:46 +0900

    better to hardcode

--- src/components/molecules/input-block.tsx
+++ src/components/molecules/input-block.tsx
@@ -1,34 +1,7 @@
 import React from 'react'
 import styled from 'styled-components'
-import * as inputLabel from '../../const/input-label'
+import * as inputId from '../../const/input-label'
 import { InputSet } from '../atoms/input-set'
-
-const inputBlockItems = [
-  {
-    inputId: inputLabel.PLAYER_NAME,
-    labelText: 'プレイヤー：'
-  },
-  {
-    inputId: inputLabel.NTH_DAY,
-    labelText: '何日目：'
-  },
-  {
-    inputId: inputLabel.OPEN_RATE,
-    labelText: '始値：'
-  },
-  {
-    inputId: inputLabel.CLOSE_RATE,
-    labelText: '終値：'
-  },
-  {
-    inputId: inputLabel.LOW_PRICE,
-    labelText: '安値：'
-  },
-  {
-    inputId: inputLabel.HIGH_PRICE,
-    labelText: '高値：'
-  }
-]
 
 interface IInputBlockProps {
   playerName: string
@@ -39,45 +12,51 @@ interface IInputBlockProps {
   highPriceValue: number
 }
 
-const getInputValue = (inputId: string, props: IInputBlockProps) => {
-  switch (inputId) {
-    case inputLabel.PLAYER_NAME:
-      return {
-        type: 'text',
-        value: props.playerName,
-        placeholder: 'デモ太郎'
-      }
-    case inputLabel.NTH_DAY:
-      return { type: 'number', value: props.nthDayValue, placeholder: '' }
-    case inputLabel.OPEN_RATE:
-      return { type: 'number', value: props.openRateValue, placeholder: '' }
-    case inputLabel.CLOSE_RATE:
-      return { type: 'number', value: props.closeRateValue, placeholder: '' }
-    case inputLabel.LOW_PRICE:
-      return { type: 'number', value: props.lowPriceValue, placeholder: '' }
-    case inputLabel.HIGH_PRICE:
-      return { type: 'number', value: props.highPriceValue, placeholder: '' }
-    default:
-      return { type: 'text', value: '', placeholder: '' }
-  }
-}
-
 export const InputBlock = (props: IInputBlockProps) => {
   return (
     <Wrapper>
-      {inputBlockItems.map(({ inputId, labelText }) => {
-        const { type, value, placeholder } = getInputValue(inputId, props)
-        return (
-          <InputSet
-            key={inputId}
-            inputId={inputId}
-            type={type}
-            value={value}
-            placeholder={placeholder}
-            labelText={labelText}
-          />
-        )
-      })}
+      <InputSet
+        inputId={inputId.PLAYER_NAME}
+        type='text'
+        value={props.playerName}
+        placeholder='デモ太郎'
+        labelText='プレイヤー：'
+      />
+      <InputSet
+        inputId={inputId.NTH_DAY}
+        type='number'
+        value={props.nthDayValue}
+        placeholder=''
+        labelText='何日目：'
+      />
+      <InputSet
+        inputId={inputId.OPEN_RATE}
+        type='number'
+        value={props.openRateValue}
+        placeholder=''
+        labelText='始値：'
+      />
+      <InputSet
+        inputId={inputId.CLOSE_RATE}
+        type='number'
+        value={props.closeRateValue}
+        placeholder=''
+        labelText='終値：'
+      />
+      <InputSet
+        inputId={inputId.LOW_PRICE}
+        type='number'
+        value={props.lowPriceValue}
+        placeholder=''
+        labelText='安値：'
+      />
+      <InputSet
+        inputId={inputId.HIGH_PRICE}
+        type='number'
+        value={props.highPriceValue}
+        placeholder=''
+        labelText='高値：'
+      />
     </Wrapper>
   )
 }

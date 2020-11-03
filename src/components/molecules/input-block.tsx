415a5c7e765b87676f3b7d5aca08666a98ce07cb
Sungjoon Park <sungjoon512@gmail.com>
2020-11-03 17:10:05 +0900

    use const

--- src/components/molecules/input-block.tsx
+++ src/components/molecules/input-block.tsx
@@ -1,37 +1,31 @@
 import React from 'react'
 import styled from 'styled-components'
+import * as inputLabel from '../../const/input-label'
 import { InputSet } from '../atoms/input-set'
-
-const PLAYER_NAME = 'playerName'
-const NTH_DAY = 'nthDay'
-const OPEN_RATE = 'openRate'
-const CLOSE_RATE = 'closeRate'
-const LOW_PRICE = 'lowPrice'
-const HIGH_PRICE = 'highPrice'
 
 const inputBlockItems = [
   {
-    id: PLAYER_NAME,
+    id: inputLabel.PLAYER_NAME,
     labelText: 'プレイヤー：'
   },
   {
-    id: NTH_DAY,
+    id: inputLabel.NTH_DAY,
     labelText: '何日目：'
   },
   {
-    id: OPEN_RATE,
+    id: inputLabel.OPEN_RATE,
     labelText: '始値：'
   },
   {
-    id: CLOSE_RATE,
+    id: inputLabel.CLOSE_RATE,
     labelText: '終値：'
   },
   {
-    id: LOW_PRICE,
+    id: inputLabel.LOW_PRICE,
     labelText: '安値：'
   },
   {
-    id: HIGH_PRICE,
+    id: inputLabel.HIGH_PRICE,
     labelText: '高値：'
   }
 ]
@@ -47,21 +41,21 @@ interface IInputBlockProps {
 
 const getInputValue = (id: string, props: IInputBlockProps) => {
   switch (id) {
-    case PLAYER_NAME:
+    case inputLabel.PLAYER_NAME:
       return {
         type: 'text',
         value: props.playerName,
         placeholder: 'デモ太郎'
       }
-    case NTH_DAY:
+    case inputLabel.NTH_DAY:
       return { type: 'number', value: props.nthDayValue, placeholder: '' }
-    case OPEN_RATE:
+    case inputLabel.OPEN_RATE:
       return { type: 'number', value: props.openRateValue, placeholder: '' }
-    case CLOSE_RATE:
+    case inputLabel.CLOSE_RATE:
       return { type: 'number', value: props.closeRateValue, placeholder: '' }
-    case LOW_PRICE:
+    case inputLabel.LOW_PRICE:
       return { type: 'number', value: props.lowPriceValue, placeholder: '' }
-    case HIGH_PRICE:
+    case inputLabel.HIGH_PRICE:
       return { type: 'number', value: props.highPriceValue, placeholder: '' }
     default:
       return { type: 'text', value: '', placeholder: '' }

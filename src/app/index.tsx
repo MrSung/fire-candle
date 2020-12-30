899b075bed52189a852c1b3b8e5d46d9b7d84197
Sungjoon Park <sungjoon512@gmail.com>
2020-12-30 21:34:08 +0900

    add reference firebase methods

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,12 +1,29 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import styled from 'styled-components'
 
+import { useFirebase } from './firebase'
+// import { initialChartValue } from './features/charts/charts-slice'
 import { InputChartBlocks } from '../components/organisms/input-chart-blocks'
 
-export const App = () => (
-  <Wrapper>
-    <InputChartBlocks />
-  </Wrapper>
-)
+export const App = () => {
+  const firebase = useFirebase()
+
+  useEffect(() => {
+    if (firebase === null) return
+
+    // void firebase.referDbByKeyName('charts').push(initialChartValue, () => {
+    //   // eslint-disable-next-line no-console
+    //   console.log('initialChartValue pushed!')
+    // })
+
+    // void firebase.referDbByKeyName('charts').once('value', snapshot => {
+    //   const data = snapshot.val()
+    //   // eslint-disable-next-line no-console
+    //   console.log('data', data)
+    // })
+  }, [firebase])
+
+  return <Wrapper>{firebase !== null && <InputChartBlocks />}</Wrapper>
+}
 
 const Wrapper = styled.div``
